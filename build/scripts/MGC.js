5957e23da6b823b8daf2c894441a3c49a55f908b
Matthew Cobbs <draeton@gmail.com>
2010-05-28 23:55:18 -0400

    initial build

---
+++ build/scripts/MGC.js
@@ -0,0 +1,30 @@
+/*!
+ * MGC
+ * http://matthewcobbs.com/
+ *
+ * Main site execution control script
+ *
+ * Copyright (c) 2010 Matthew Cobbs
+ *
+ */
+/*global window, document, jQuery, $, MGC */
+MGC = window.MGC || {};
+
+(function (MGC, $) {
+    // cufon type replacement
+    if (typeof Cufon !== 'undefined') {
+        Cufon.replace('.title');
+        Cufon.now();
+    }
+
+    $(document).ready(function () {
+
+        if (MGC.hasOwnProperty('main')) {
+
+            var main = new MGC.main();
+            main.init();
+
+        }
+        
+    });
+}(MGC, jQuery));
